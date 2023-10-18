cfc7e95746b8b724b31f77ddc8b36a6a9b8bc1a1
Rifazul <rifazul50@gmail.com>
2023-10-18 07:03:36 +0600

    add the banner section

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,20 @@
+
+const Banner = () => {
+
+return (
+<div className="hero min-h-screen" style={{backgroundImage: 'url(/banner3.jpeg)'}}>
+  <div className="hero-overlay bg-opacity-10"></div>
+  <div className="hero-content  text-neutral-content">
+    <div className=" lg:pl-[700px]">
+    <div className="max-w-md ">
+      <h1 className="mb-5 text-5xl font-bold">The New Brand</h1>
+      <p className="mb-5">The New Brand is more than just a car; it s a symbol of innovation, style, and cutting-edge technology in the automotive world.</p>
+      <button className="btn btn-secondary capitalize">read more ..</button>
+    </div>
+    </div>
+  </div>
+</div>
+ );
+};
+
+export default Banner;
